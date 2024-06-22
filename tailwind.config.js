a0888666f8b3a28307286c39042aa5d25e547014
Nokibulislam27 <nokibulislamanas27@gmail.com>
2024-06-22 14:47:49 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    daisyui: {
+      themes: ["light", "dark", "cupcake"],
+    },
+  },
+  plugins: [
+    require('daisyui')
+  ],
+});
